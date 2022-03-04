17a580ffcebdb9eb057a0bb810b84a5bf77d899e
KyleStenb <kyle.stenberg@ndsu.edu>
2022-03-04 11:53:23 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-bi3hxr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-bi3hxr)
---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name = 'Bob';
+  itemImageURL: string =
+    'https://www.ndsu.edu/fileadmin/www.ur.ndsu.edu/logo_usage/NDSU.logo.typebox.jpg';
+  isSpecial: boolean = true;
+
+  onMouseOver(): void {
+    this.isSpecial = !this.isSpecial;
+  }
+
+  onMouseOut(): void {
+    this.isSpecial = !this.isSpecial;
+  }
+
+  onClick(): boolean{
+    return this.username == 'helloworld';
+  }
+}
